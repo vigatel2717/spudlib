441c89fb7090f020e0cf4f907bda39897ba5f868
vigatel2717 <vigatel2717@gmail.com>
2026-05-20 10:46:22 -0400

    Merge remote-tracking branch 'origin/master'

--- bindings/rust/spudlib-sys/build.rs
+++ bindings/rust/spudlib-sys/build.rs
@@ -30,7 +30,7 @@ fn main() {
         build.define("SPUDGPU_COMPILE_METAL_API", "1");
     }
 
-    build.compile("spudlib-sys");
+    build.compile("spudlib");
 
     // ----------------------------------------------------------------
     // 2. Link platform Vulkan library
